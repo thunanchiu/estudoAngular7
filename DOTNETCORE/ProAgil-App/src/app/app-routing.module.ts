8509e510a15bbbd288b6fdba627a947e74903ad1
thunanchiu <thunanchiu@hotmail.com>
2019-08-29 15:09:20 -0300

    Ajustes na rota e configuração dos botões da NAV para redirecionar para cada componente quando for clicado.

--- DOTNETCORE/ProAgil-App/src/app/app-routing.module.ts
+++ DOTNETCORE/ProAgil-App/src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
   {path: 'eventos', component: EventosComponent},
   {path: 'palestrantes', component: PalestrantesComponent},
   {path: 'dashboard', component: DashboardComponent},
-  {path: 'contatos', component: ContatosComponent}
+  {path: 'contatos', component: ContatosComponent},
+  //Caso nenhuma rota for atribuida no endereço, será redirecionado para o dashboard.
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  //Se qualquer rota no endereço não iver dado match nas rotas a cima, será redirecionado para o dashboard.
+  {path: '**',  redirectTo: 'dashboard', pathMatch: 'full'}
 ];
 
 @NgModule({
